aeaa96765cbd5a6e7062a8131fa4eb8c571344a1
Rick Brown <ricky.brown.00@gmail.com>
2021-06-03 13:35:47 +0100

    Initial Commit

---
+++ SwiftUI-MVVM-Example/SwiftUI_MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_ExampleApp.swift
+//  SwiftUI-MVVM-Example
+//
+//  Created by Rick Brown on 03/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
